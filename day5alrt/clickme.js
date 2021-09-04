088188f2a55842e4817025840dcbfc1a17c6cf4e
macpro <nebiyugirma7497@yahoo.com>
2021-09-03 23:27:57 -0500

    updajhhte

--- day5alrt/clickme.js
+++ day5alrt/clickme.js
@@ -1,3 +1,14 @@
-function clickme(){
-    alert("you click me")
+function calcTip() {
+	var subtotalElem = document.getElementById('subtotal')
+	var tipElem =document.getElementById('tip') 
+	var totalElem = document.getElementById('total');
+    if(subtotalElem.value==""){
+        alert("enter amount")
+    }
+    else{
+        var total=document.getElementById("total");
+        var tipvalue=(subtotalElem.value*tipElem.value)/100;
+        totalElem.innerHTML='s'+(parseInt(subtotalElem.value)+tipvalue);
+    }
+
 }
