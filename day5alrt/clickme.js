4b5794bddd18e2683cfcbc2531f3110fd67ae9ab
macpro <nebiyugirma7497@yahoo.com>
2021-09-06 10:30:33 -0500

    updwe

--- day5alrt/clickme.js
+++ day5alrt/clickme.js
@@ -1,14 +1,3 @@
-function calcTip() {
-	var subtotalElem = document.getElementById('subtotal')
-	var tipElem =document.getElementById('tip') 
-	var totalElem = document.getElementById('total');
-    if(subtotalElem.value==""){
-        alert("enter amount")
-    }
-    else{
-        var total=document.getElementById("total");
-        var tipvalue=(subtotalElem.value*tipElem.value)/100;
-        totalElem.innerHTML='s'+(parseInt(subtotalElem.value)+tipvalue);
-    }
-
+function clickme(){
+    alert("you Click me");
 }
